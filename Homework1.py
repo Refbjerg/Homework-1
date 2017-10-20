7ac4e12a7e22ddb6dc9723d3128ea7df83ea0c8c
Refbjerg <wooler@bk.ru>
2017-10-20 20:46:57 +0400

    Добавлены комментарии. Выполнен рефакторинг методов для удобности чтения кода.

--- Homework1.py
+++ Homework1.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import unittest
 
 def is_alert_present(wd):
     try:
@@ -18,14 +17,19 @@ class Homework1(unittest.TestCase):
     def test_Homework1(self):
         success = True
         wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("Admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        self.open_homepage(wd)
+        #login
+        self.login(wd, "Admin", "secret")
+        self.group_creation(wd)
+        self.Submit_group(wd)
+        self.assertTrue(success)
+
+    def Submit_group(self, wd):
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+
+    def group_creation(self, wd):
+        # group creation
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -36,9 +40,20 @@ class Homework1(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("Groupfooter")
-        wd.find_element_by_name("submit").click()
-        self.assertTrue(success)
-    
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_homepage(self, wd):
+        # open homepage
+        wd.get("http://localhost/addressbook/group.php")
+
     def tearDown(self):
         self.wd.quit()
 
