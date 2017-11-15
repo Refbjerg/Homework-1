5054b77620d2cb1535054edcb91cc9001526b743
Refbjerg <wooler@bk.ru>
2017-11-15 17:38:09 +0400

    Выделен отдельный класс, представляющий сложную фикстуру.

--- Homework1.py
+++ Homework1.py
@@ -1,73 +1,29 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
 from group import Group
+from application import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
-class Homework1(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-    
-    def test_Homework1(self):
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.Destroy)
+    return fixture
+
+
+def test_Homework1(app):
         success = True
-        wd = self.wd
-        self.open_homepage(wd)
-        #login
-        self.login(wd, username="Admin", password="secret")
-        self.group_creation(wd, Group(name="Group-1", header="Groupleader", footer="Groupfooter"))
-        self.Submit_group(wd)
-        self.assertTrue(success)
+        app.login(username="Admin", password="secret")
+        app.group_creation(Group(name="Group-1", header="Groupleader", footer="Groupfooter"))
+        app.Submit_group()
 
-    def test_Homework1_empty(self):
+
+def test_Homework1_empty(app):
         success = True
-        wd = self.wd
-        self.open_homepage(wd)
-        #login
-        self.login(wd, username="admin", password="secret")
-        self.group_creation(wd, Group(name="", header="", footer=""))
-        self.Submit_group(wd)
-        self.assertTrue(success)
+        app.login(username="admin", password="secret")
+        app.group_creation(Group(name="", header="", footer=""))
+        app.Submit_group()
 
-
-    def Submit_group(self, wd):
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-
-    def group_creation(self, wd, group):
-        # group creation
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_homepage(self, wd):
-        # open homepage
-        wd.get("http://localhost/addressbook/group.php")
-
-    def tearDown(self):
-        self.wd.quit()
 
 if __name__ == '__main__':
     unittest.main()
---
+++ application.py
@@ -0,0 +1,49 @@
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+class Application:
+
+    def __init__(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(60)
+    def Submit_group(self):
+        wd = self.wd
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+
+    def group_creation(self, group):
+        # group creation
+        wd = self.wd
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+
+    def login(self, username, password):
+        wd = self.wd
+        self.open_homepage()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_homepage(self):
+        wd = self.wd
+        # open_homepage
+        wd.get("http://localhost/addressbook/group.php")
+
+    def tearDown(self):
+        self.wd.quit()
+
+    def Destroy(self):
+        self.wd.quit()
+if __name__ == '__main__':
+    unittest.main()
